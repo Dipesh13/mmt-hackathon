426e7aa751dd1864edddacc5b2754271a8342d6e
Dipesh13 <mangwani.dipesh@gmail.com>
2018-06-17 09:51:43 +0530

    separate models for processing numeric and text data.

--- only_numeric_columns.py
+++ only_numeric_columns.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import json
-from sklearn.base import TransformerMixin
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import Imputer
@@ -14,45 +13,22 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.preprocessing import FunctionTransformer
-from sklearn.pipeline import FeatureUnion
-
-class DenseTransformer(TransformerMixin):
-
-    def transform(self, X, y=None, **fit_params):
-        return X.todense()
-
-    def fit_transform(self, X, y=None, **fit_params):
-        self.fit(X, y, **fit_params)
-        return self.transform(X)
-
-    def fit(self, X, y=None, **fit_params):
-        return self
 
 df = pd.read_csv('train.csv')
 with open('config_file.json','rb') as fi:
     config_dict = json.load(fi)
 
-text_cols = ['A','D','E','F','G','I','J','L','M']
+num_cols = ['B','C','H','K','N','O']
 
-def combine_text_columns(data_frame):
-    # next line handles missing text values.
-    text_data.fillna("", inplace=True)
-    return text_data.apply(lambda x: " ".join(x), axis=1)
-
-
-text_data = df[text_cols]
-text_data = combine_text_columns(text_data)
-# print(text_data)
+num_data = df[num_cols]
 y = df['P']
 
-X_train, X_test, y_train, y_test = train_test_split(text_data, y, test_size = 0.2, random_state=42, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(num_data, y, test_size = 0.2, random_state=42, stratify=y)
+
 
 for key,value in config_dict.items():
     clf = Pipeline([
-        ('vec', CountVectorizer()),
-        ('to_dense', DenseTransformer()),
+        ('imputer', Imputer()),
         ('model',eval(value))
     ])
     clf.fit(X_train,y_train)
